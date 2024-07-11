25b9f3e9b1fb183063db8e1223dc058bac894f48
ShantanuBhatt2001 <113619498+Shantanu-Bhatt-K@users.noreply.github.com>
2024-07-11 18:02:17 +0100

    CPU Perlin Worms Creation

--- Assets/Cave Generation/Ontogenetic/Perlin Noise/CPU Generation/Scripts/GenerateNoise.cs
+++ Assets/Cave Generation/Ontogenetic/Perlin Noise/CPU Generation/Scripts/GenerateNoise.cs
@@ -4,7 +4,8 @@ public enum NoiseType
     CPU2D,
     CPU3D,
     CPU3D_Multi,
-    GPU_3D
+    GPU_3D,
+    Worm3D
 }
 public class GenerateNoise : MonoBehaviour
 {
@@ -18,9 +19,16 @@ public class GenerateNoise : MonoBehaviour
     public int octaves;
     public float lacunarity;
     public float persistance;
-    
+    public ComputeShader noiseShader;
     [Range(0f, 1f)]
-    public float cutoff = 0.5f;
+    public float lower_cutoff = 0.5f;
+    [Range(0f, 1f)]
+    public float upper_cutoff = 0.6f;
+
+    [Header("Worm Data")]
+    public int wormCount=10;
+    public int wormLength = 30;
+    public float wormRadius = 3f;
     
     public void GenerateMap()
     {
@@ -35,15 +43,25 @@ public class GenerateNoise : MonoBehaviour
             case NoiseType.CPU3D:
                 float[,,] meshData= Perlin_3d_Calc.MeshData_Gen(width, height, depth,scale,seed,octaves,lacunarity,persistance);
                 if(RenderNoise)
-                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshData, new Vector3(width, height, depth),cutoff);
+                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshData, new Vector3(width, height, depth),lower_cutoff);
                 break;
             case NoiseType.CPU3D_Multi:
                 float[,,] meshDataMulti = Perlin_3D_Multi.MeshDataGen(width, height, depth, scale, seed, octaves, lacunarity, persistance);
                 if (RenderNoise)
-                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshDataMulti, new Vector3(width, height, depth), cutoff);
+                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshDataMulti, new Vector3(width, height, depth), lower_cutoff);
                 break;
             case NoiseType.GPU_3D:
+                float[,,] meshDataGPU = Perlin_3D_GPU.MeshDataGen(width, height, depth, scale, seed, octaves, lacunarity, persistance,noiseShader);
+                if (RenderNoise)
+                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshDataGPU, new Vector3(width, height, depth), lower_cutoff);
                 break;
+            case NoiseType.Worm3D:
+                float[,,] meshData_Worm=new float[width,height,depth];
+                PerlinWormCPU.MeshData_Gen(width, height, depth, scale, seed, octaves, lacunarity, persistance, ref meshData_Worm, wormCount, wormLength, wormRadius);
+                if (RenderNoise)
+                    gameObject.GetComponent<PerlinRenderer>().RenderCPU3D(meshData_Worm, new Vector3(width, height, depth), lower_cutoff);
+                break;
+
             default:
                 break;
 
